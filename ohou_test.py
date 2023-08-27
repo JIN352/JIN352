3a8fda93035454f3eec6ce1820023f301fb57c9c
JIN352 <dlthdudzj@gmail.com>
2023-08-27 17:55:10 +0900

    STEP3 수정 필요

--- ohou_test.py
+++ ohou_test.py
@@ -33,8 +33,14 @@ for number in range(5):
     item = random.choice(itemlist)
     url = 'https://ohou.se/productions/' + item
     driver.get(url)
+    time.sleep(10)
     xpath = '/html/body/div[1]/div'
-    itemname = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, xpath))).get_attribute('production-selling-header__title__name')
+    #itemname = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, xpath))).get_attribute('production-selling-header__title__name')
+    itemname = driver.find_element(By.CLASS_NAME, 'production-selling-header__title__name-wrap')
+    baner = driver.find_element(By.CLASS_NAME, 'production-selling-header__title__brand-wrap')
+    #baner = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, xpath))).get_attribute('production-selling-header__title__brand')
+    print(str(itemname))
+    print(baner)
 
     #4.장바구니 버튼 클릭
     print('장바구니버튼 클릭')
@@ -47,12 +53,36 @@ for number in range(5):
         alert = driver.switch_to.alert
         alert.accept()
         xpath = '/html/body/div[1]/div/div/div[2]/div[1]/div/div[2]/div[2]/section/div/div/div/select'
-        element = Select(driver.find_element(By.XPATH, xpath)).getattribute('value')
-        element.select_by_value('1')
+        element = driver.find_element(By.XPATH, xpath)
+        Select(element).select_by_value('0')
         element = driver.find_element(By.XPATH, xpath).click()
-        time.sleep(10)
+        time.sleep(3)
+
+        try:
+            WebDriverWait(driver, 3).until(EC.alert_is_present())
+            alert = driver.switch_to.alert
+            alert.accept()
+            xpath = '/html/body/div[5]/div/div/div/section/div/div/div[2]/select'
+            element = driver.find_element(By.XPATH, xpath)
+            Select(element).select_by_value('0')
+            element = driver.find_element(By.XPATH, xpath).click()
+            time.sleep(3)
+
+        except: pass
 
     except: pass
+
+    #5.장바구니 이동
+    xpath = '/html/body/div[1]/div/div/header/div[1]/div/div/div[4]/div/a'
+    element = driver.find_element(By.XPATH, xpath).click()
+
+    #6.상품 확인
+    xpath = '/html/body/div[1]/div/div/div[2]/div[1]/div/div[1]/div/ul/li[1]/article/ul/li[1]/article/ul/li/article/a/div[2]/h1'
+    element = driver.find_element(By.XPATH, xpath).text()
+    print(itemname)
+    print(baner)
+
+
 
 
 # xpath = '/html/body/div[5]/div/div/div/div/div[1]/h2'

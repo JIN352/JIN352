064be9f82b6636d4b1470771b27468946fe02b00
JIN352 <dlthdudzj@gmail.com>
2023-08-27 15:06:01 +0900

    랜덤 상품 반복 진입

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import random
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -6,9 +8,10 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Chrome()
+number=0
 
-# 비회원 구매 팝업 노출 확인
-#1.네이버 이동
+# 랜덤 상품 장바구니 담기 확인
+#1.브라우저 연결
 print('브라우저 연결')
 url = 'https://ohou.se/'
 driver.get(url)
@@ -20,26 +23,34 @@ xpath = '/html/body/div[1]/div/div/header/div[1]/div/div/div[3]/a[2]'
 element = driver.find_element(By.XPATH, xpath).click()
 
 #3.임의의 상품 선택
-print('상품페이지 이동')
-xpath = '//*[@id="store-index"]/section[3]/div[2]/div[5]/article/a'
-element=WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, xpath))).get_attribute('href')
-driver.get(element)
+for number in range(5):
 
-#4.장바구니 버튼 클릭
-print('장바구니버튼 클릭')
-xpath = '/html/body/div[1]/div/div/div[2]/div[1]/div/div[2]/div[2]/div/button[1]'
-element = driver.find_element(By.XPATH, xpath).click()
+    print('상품페이지 이동'+str(number))
+    itemlist = ['1244849','1297593','1502238','767440','1450530','1918219']
+    item = random.choice(itemlist)
+    url = 'https://ohou.se/productions/'+ item
+    driver.get(url)
+    xpath = '/html/body/div[1]/div'
+    itemname = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, xpath))).get_attribute('production-selling-header__title__name')
 
-try:
-    WebDriverWait(driver, 3).until(EC.alert_is_present())
-    alert = driver.switch_to.alert
-    alert.accept()
-    xpath = '/html/body/div[1]/div/div/div[2]/div[1]/div/div[2]/div[2]/section/div/div/div/select'
-    element = driver.find_element(By.XPATH, xpath)
-    element.select_by_value('1')
-    time.sleep(10)
+    #4.장바구니 버튼 클릭
+    print('장바구니버튼 클릭')
+    xpath = '/html/body/div[1]/div/div/div[2]/div[1]/div/div[2]/div[2]/div/button[1]'
+    element = driver.find_element(By.XPATH, xpath).click()
+    time.sleep(3)
 
-except: pass
+    try:
+        WebDriverWait(driver, 3).until(EC.alert_is_present())
+        alert = driver.switch_to.alert
+        alert.accept()
+        xpath = '/html/body/div[1]/div/div/div[2]/div[1]/div/div[2]/div[2]/section/div/div/div/select'
+        element = driver.find_element(By.XPATH, xpath)
+        element.select_by_value('1')
+        time.sleep(10)
+
+    except: pass
+
+time.sleep(15)
 
 
 # xpath = '/html/body/div[5]/div/div/div/div/div[1]/h2'

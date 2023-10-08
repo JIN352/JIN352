9f27f09604f472f5e33a017ab7a5e892ee69ab13
JIN352 <dlthdudzj@gmail.com>
2023-10-08 16:15:08 +0900

    pytest-rerunfailures 추가

--- test_main.py
+++ test_main.py
@@ -1,13 +1,16 @@
 import ohou_test
+import pytest
 
-#@pytest.mark.flaky(reruns=3, reruns_delay=2) # 실패 후 3번 재 실행
+@pytest.mark.flaky(reruns=3, reruns_delay=2) # 실패 후 3번 재 실행
 # 랜덤 상품 장바구니 담기 확인
-try:
-    ohou_test.test_1()
-    print('OHO-1 Pass')
-except: print('OHO-1 Fail')
+def test():
 
-try:
-    ohou_test.test_2()
-    print('OHO-2 Pass')
-except: print('OHO-2 Fail')
+    try:
+        ohou_test.test_1()
+        print('OHO-1 Pass')
+    except: print('OHO-1 Fail')
+
+    try:
+        ohou_test.test_2()
+        print('OHO-2 Pass')
+    except: print('OHO-2 Fail')

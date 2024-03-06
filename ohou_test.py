74ca16673e546926a4ce036b0551784a6867ef78
JIN352 <dlthdudzj@gmail.com>
2024-03-06 20:35:55 +0900

    ohou_test.py test_2 - 스크린샷 저장 구문 순서 변경 test_3 - 변수명 변경 itemname -> item_list STEP5 수정, STEP6 수정, STEP7 수정

--- ohou_test.py
+++ ohou_test.py
@@ -106,6 +106,7 @@ def test_2():
         xpath_s = '/html/body/div[1]/div/div/header/div/div/div/div[4]/div/a/span[2]'
         element_s = driver.find_element(By.XPATH, xpath_s).text         #상단 장바구니 아이콘에 담긴 상품 노출 개수 확인
         count_s = str(count)
+        test_func.cart_screenshot(folder + 'OHO-2')                     #장바구니 내부 이미지 저장
         if count_s == element_s:                                        #실제 담은 상품 수 와 장바구니 아이콘에 노출된 상품 개수 비교
             f.write('장바구니에 담은 상품 수: ' + count_s + '\n')
             f.write('장바구니 아이콘에 노출된 상품 수: ' + element_s + '\n')
@@ -115,12 +116,12 @@ def test_2():
             f.write('Faill - 장바구니에 담긴 상품 수 확인\n')
 
         f.close()
-        test_func.cart_screenshot(folder + 'OHO-2')          #장바구니 내부 이미지 저장
 
         if count_s == element_s:                            #TEST 결과 반환
             print('test2 pass')
             return True
         else:return False
+
     except:return False
 
 def test_3():
@@ -129,7 +130,7 @@ def test_3():
         driver.delete_all_cookies()  # 쿠키 삭제
         f = open("C:/test/ohou_test_result.txt", 'a')
         f.write('\n\nOHO-3:장바구니 상품 삭제 기능 확인\n')
-        itemname=[]
+        item_list=[]
 
         # 1.오늘의집 홈페이지 이동
         url = 'https://ohou.se/'
@@ -145,7 +146,7 @@ def test_3():
             xpath = '//*[@id="store-index"]/section[3]/div[2]/div[' + str(number) + ']/article/a'       #상품링크 추출
             element = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, xpath))).get_attribute('href')
             driver.get(element)         #상품 페이지 이동
-            itemname.append(test_func.item_name())
+            item_list.append(test_func.item_name())
             test_func.item_option()     #상품 장바구니에 담기
             clk_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, f"//*[text()='쇼핑']")))
             clk_button.click() #쇼핑탭 이동
@@ -156,23 +157,21 @@ def test_3():
         time.sleep(2)
 
         # 5.상품 확인
-        count_s = count
-        a = 0
-        f.write('장바구니에 담김 상품 수: ' + str(count) + '\n')
+        count_s = len(driver.find_elements(By.CLASS_NAME, 'commerce-cart__delivery-group__product-item'))
+        f.write('장바구니에 담김 상품 수: ' + str(count_s) + '\n')
         test_func.cart_screenshot(folder + 'OHO-3_before')  # 장바구니 내부 이미지 저장
         while count_s >= 1:                                 #장바구니내 상품 이름 추출
-            driver.implicitly_wait(3)
-            name_xpath = '/html/body/div[1]/div/div/div[2]/div[1]/div/div[1]/div/ul/li['+ str(count_s) +']/article/ul/li/article/ul/li/article/a/div[2]/h1'
-            element = driver.find_element(By.XPATH, name_xpath).text
-
-            if itemname[a] == element:                    #담은 상품명과 장바구니내 노출 상품명 확인
-                pass
-            else:
-                f.write('Fail - 장바구니에 담긴 상품 확인(상품명 불일치)\n')
-                f.write('담은 상품: '+itemname+' / 노출 상품: '+ element)
-                return False
             count_s -=1
-            a +=1
+            name_xpath = 'product-small-item__title'
+            element = driver.find_elements(By.CLASS_NAME, name_xpath)[count_s].text
+
+            for itme_name in item_list:
+                if element == itme_name:                    #담은 상품명과 장바구니내 노출 상품명 확인
+                    break
+            else:
+                f.write('담은 상품: ' + itme_name + ' / 노출 상품: ' + element+'\n')
+                f.write('Fail - 장바구니에 담긴 상품 확인(상품명 불일치)\n')
+                return False
 
         # 6.임의의 상품 삭제
         if count < 3:                                               #삭제할 상품 개수 설정
@@ -180,10 +179,9 @@ def test_3():
         else:
             number_del = random.randrange(1, count - 1)
 
-        itemname = ["default_value"] * number_del                   #리스트 초기화
-        for count_s in range(1, number_del+1):                      #삭제할 상품의 상품명 확인
-            name_xpath = '/html/body/div[1]/div/div/div[2]/div[1]/div/div[1]/div/ul/li['+str(count_s)+']/article/ul/li/article/ul/li/article/a/div[2]/h1'
-            itemname[(count_s-1)] = driver.find_element(By.XPATH, name_xpath).text
+        item_list = [''] * number_del                   #리스트 초기화
+        for a in range(0, number_del):                      #삭제할 상품의 상품명 확인
+            item_list[a] = driver.find_elements(By.CLASS_NAME, name_xpath)[a].text
 
         element = driver.find_elements(By.CLASS_NAME, '_3UImz')
         for a in range(number_del + 1):                             #삭제 할 상품 수만큼 상품 선택
@@ -206,21 +204,20 @@ def test_3():
             pass
         driver.refresh()
 
-        count_s = (count - number_del)          #장바구니에 남은 상품 수
+        test_func.cart_screenshot(folder + 'OHO-3_after')  # 장바구니 내부 이미지 저장
+
+        count_s = len(driver.find_elements(By.CLASS_NAME, 'commerce-cart__delivery-group__product-item'))        #장바구니에 남은 상품 수
         while count_s >=1:
-            name_xpath = '/html/body/div[1]/div/div/div[2]/div[1]/div/div[1]/div/ul/li[' + str(count_s) + ']/article/ul/li/article/ul/li/article/a/div[2]/h1'
-            element = driver.find_element(By.XPATH, name_xpath).text  # 장바구니내 남은 상품 상품명 확인
-            for a in itemname :
-                if a == element:  # 남은 상품의 상품명과 삭제한 상품의 상품명 비교
-                    f.write('Fail - 삭제 실패 상품 확인\n')
-                    return False
-                else:pass
             count_s -=1
+            element = driver.find_elements(By.CLASS_NAME, name_xpath)[count_s].text  # 장바구니내 남은 상품 상품명 확인
+            if element in item_list:                                         # 남은 상품의 상품명과 삭제한 상품의 상품명 비교
+                f.write('Fail - 삭제 실패 상품 확인\n')
+                return False
+            else:pass
 
         f.write('남은 상품 수: ' + str(count - number_del)+'\n')
         f.write('Pass')
         f.close()
-        test_func.cart_screenshot(folder + 'OHO-3_after')           #장바구니 내부 이미지 저장
         print('test3 pass')
         return True
     except:return False
@@ -264,18 +261,19 @@ def test_4():
         total_cost = int(re.sub(r'[^0-9]', '', element))
         element = driver.find_elements(By.XPATH, '//*[contains(text(),"총 배송비")]/../dd/span')[0].get_attribute('innerText')
         ship_cost = int(re.sub(r'[^0-9]', '', element))
-        if (item_costs + ship_cost) == total_cost:                                                  #상품별 합산 금액과 장바구니에 노출되는 총 결제 금액 확인
+        cost = item_costs + ship_cost
+        if cost == total_cost:                                                  #상품별 합산 금액과 장바구니에 노출되는 총 결제 금액 확인
             f.write('노출금액:' + str(total_cost) + '\n')
             f.write('실제금액:(상품금액:' + str(item_costs) + ' / 총 배송비:' + str(ship_cost) + ')'+'\n')
             f.write('Pass')
             print('test4 pass')
         else:
             f.write('노출금액:' + str(total_cost) + '\n')
-            f.write('실제금액:(상품금액:' + str(item_costs) + ' / 총 배송비:' + str(ship_cost) + ')')
+            f.write('실제금액:(상품금액:' + str(item_costs) + ' / 총 배송비:' + str(ship_cost) + ')'+'\n')
             f.write('False\n')
 
         f.close()
-        if (item_costs + ship_cost) == total_cost:          #TEST 결과 반환
+        if cost == total_cost:          #TEST 결과 반환
             return True
         else:
             return False
